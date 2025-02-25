2c6de117e69cbffde696e2deb6a0bccb629f188b
Clement <clementwa01@gmail.com>
2025-02-25 11:07:54 +0300

    Initial commit

---
+++ Var.py
@@ -0,0 +1,13 @@
+# variables
+age = 25 #int (integer)
+height = 5.75 # float(dec)
+name = "Charlie" #str
+is_student = True #bool
+
+# printing values 
+print("Name:", name) # Outputs: Name: Charlie
+print("Age:", age) # Outputs: Age: 25
+print("Height:", height) # Outputs: Height: 5.75
+print ("Is a student?", is_student)  # Outputs: Is a student? True
+
+# "print(type(age))" # checks variable datatype
