040cbf194b075cd1ec1307ff569f37fb628f1a9a
Clement <clementwa01@gmail.com>
2025-04-09 22:34:13 +0300

    Fuctions in Python

--- python/Function/Fuction.py
+++ python/Function/Fuction.py
@@ -1,12 +1,5 @@
 def calculate_discount(price, discount_percent):
-    """
-    Calculate the final price after applying a discount.
-    If the discount is 20% or higher, apply the discount; otherwise, return the original price.
-
-    :param price: Original price of the item
-    :param discount_percent: Discount percentage to be applied
-    :return: Final price after applying the discount or the original price
-    """
+   
     if discount_percent >= 20:
         discount_amount = price * (discount_percent / 100)
         return price - discount_amount
