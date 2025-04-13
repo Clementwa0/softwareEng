4136acabc691bc42455a02a5c464a2ad5faf3187
Clement <clementwa01@gmail.com>
2025-04-13 17:18:16 +0300

    python programming

--- python/Function/Fuction.py
+++ python/Function/Fuction.py
@@ -1,3 +1,7 @@
+original_price = float(input("Enter the original price of the item: "))
+discount_percentage = float(input("Enter the discount percentage: "))
+
+
 def calculate_discount(price, discount_percent):
    
     if discount_percent >= 20:
@@ -6,18 +10,12 @@ def calculate_discount(price, discount_percent):
     else:
         return price
 
-# Prompt the user for input
-try:
-    original_price = float(input("Enter the original price of the item: "))
-    discount_percentage = float(input("Enter the discount percentage: "))
 
     # Calculate the final price
-    final_price = calculate_discount(original_price, discount_percentage)
+final_price = calculate_discount(original_price, discount_percentage)
 
     # Print the result
-    if discount_percentage >= 20:
+if discount_percentage >= 20:
         print(f"The final price after applying the discount is: ${final_price:.2f}")
-    else:
+else:
         print(f"No discount applied. The original price is: ${original_price:.2f}")
-except ValueError:
-    print("Invalid input. Please enter numeric values for price and discount percentage.")
